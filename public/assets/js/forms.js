5ee99ce9759e4501d5acda708dd178c78cf1d2dc
Miracle Lawrence <lawrencemiracle71@gmail.com>
2021-06-12 22:24:56 +0000

    fd

--- public/assets/js/forms.js
+++ public/assets/js/forms.js
@@ -83,6 +83,36 @@ var a = setInterval(function(){
 		
 	}, 1000);
 
+	
+	var c = setInterval(function(){
+		var expire3 = document.getElementsById('receivingact');
+		for(var i = 0; i < expire2.length; i++){
+			var countDown= new Date(expire2.innerHTML).getTime(  );
+			console.log(countDown)
+			var now = new Date().getTime(  );
+			var distance = countDown - now;
+			//console.log( distance )
+			var days = Math.floor(distance /(1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		//	var status = 'ACTIVE'
+			
+			//show them in the demo
+							document.getElementById("receivingactleft").innerHTML = " " + days + "d " + hours + "h "
+	  + minutes + "m " + seconds + "s ";
+			//document.getElementById("registerbutton").style.display= "none";
+			
+			if (distance  < 0) {
+	    		clearInterval(x);
+				document.getElementById('receivingactleft').innerHTML = "EXPIRED";
+				//document.getElementById("registerbutton").style.display= "block";
+	    		
+	  		}
+		}
+		
+	}, 1000);
+
 	console.log(expire2)
 	
 	
